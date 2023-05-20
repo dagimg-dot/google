ba5c434b22c1c2224a834b50b395dc8f2c95fcb7
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-21 01:03:20 +0300

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,40 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
-</script>
-
 <template>
-  <Header />
-  <Main />
-  <Footer />
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <div class="bg-white py-3">
+      <div class="container mx-auto flex justify-end">
+          <ul class="flex space-x-6 items-center">
+            <li><a href="#" class="text-gray-700 hover:underline">Gmail</a></li>
+            <li><a href="#" class="text-gray-700 hover:underline" >Images</a></li>
+            <a href="">
+              <div class="w-8 h-8 flex items-center justify-center">
+                <img src="../assets/9dots.svg" class="w-5 h-4">
+              </div>
+            </a>
+            <li><button class="bg-blue-500 hover:bg-blue-700 text-white py-1.5 px-6 rounded">Sign in</button></li>
+          </ul>
+      </div>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  li a { 
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 13px;
+    line-height: 26px;
+  }
+
+  a div img {
+    color: #5f6368;
+  }
+
+  ul div:hover {
+    background-color: #f1f3f4;
+    border-radius: 50%;
+  }
+
+  li button {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 14px;
+  }
 </style>
+
+ 
