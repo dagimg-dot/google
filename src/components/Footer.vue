55d78fc2d3f84c197b821982e83112ed73604fea
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-21 08:01:07 +0300

    add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<script>
+</script>
+
+<template>
+    <div class="bottom-0 fixed w-full">
+      <div class="flex items-center py-3 px-7">
+        <span>Ethiopia</span>
+      </div>
+      <div class="flex justify-between items-center border-t border-gray-300 py-3 px-8">
+        <div class="flex space-x-7">
+          <a href="#">About</a>
+          <a href="#">Advertising</a>
+          <a href="#">Business</a>
+          <a href="#">How Search works</a>
+        </div>
+        <div class="flex space-x-7">
+          <a href="#">Privacy</a>
+          <a href="#">Terms</a>
+          <a href="#">Settings</a>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  div {
+    background-color: #F2F2F2;
+  }
+  
+  a,
+  span {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 14px;
+    color: #70757a;
+  }
+
+  span {
+    font-size: 15px;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
