c9a154e36fa29ed85c34a547710c96908f5faf64
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-21 15:33:57 +0300

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
+  import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
+  import Footer from './components/Footer.vue';
+
 </script>
 
 <template>
