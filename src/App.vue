026a76c5851487ff726135e843809436032cb726
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-21 08:03:07 +0300

    remove old component reference

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 import Footer from './components/Footer.vue'
@@ -9,7 +8,6 @@ import Footer from './components/Footer.vue'
   <Header />
   <Main />
   <Footer />
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
