9b45deaec221ef2c830d6e87e006fc6f1c602dac
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-26 01:28:53 +0300

    migrate the search page to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
-  import Header from './components/Header.vue';
-  import Main from './components/Main.vue';
-  import Footer from './components/Footer.vue';
-  import { ref } from 'vue'
 
-  const isButtonClicked = ref(true)
-  
-  const getBoolean = (value) => {
-    isButtonClicked.value = value
-  }
-
+  import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <Header @button-clicked="getBoolean"/>
-  <Main :clickValue="isButtonClicked"/>
-  <Footer />
+  <RouterView />
 </template>
 
 <style scoped>  
---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import Header from '../components/Header.vue';
+  import Main from '../components/Main.vue';
+  import Footer from '../components/Footer.vue';
+  import { ref } from 'vue'
+
+  const isButtonClicked = ref(true)
+  
+  const getBoolean = (value) => {
+    isButtonClicked.value = value
+  }
+
+</script>
+
+<template>
+  <Header @button-clicked="getBoolean"/>
+  <Main :clickValue="isButtonClicked"/>
+  <Footer />
+</template>
+
+<style scoped>  
+</style>
