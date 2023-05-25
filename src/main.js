396a1cce54eefda6b84222eb1327f12d89a72e86
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-26 01:29:09 +0300

    add routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import ResultPage from './pages/ResultPage.vue'
+import SearchPage from './pages/SearchPage.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: SearchPage },
+    { path: '/search', component: ResultPage },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),    
+    routes,
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
