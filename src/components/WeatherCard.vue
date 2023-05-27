87a4d54679b7dad7712c8183e793ec91d3cb232f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-27 08:25:40 +0300

    create weather card component

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import WeatherData from '../components/WeatherData.vue';
+</script>
+
+<template>
+    <div class="weather p-3 flex flex-col">
+        <span class="weather-title">Weather</span>
+        <div class="flex justify-between pt-3 pb-3">
+            <WeatherData />
+            <WeatherData />
+            <WeatherData />
+        </div>
+        <a href="#" class="weather-site hover:underline">weather.com</a>
+    </div>
+</template>
+
+<style scoped>
+.weather {
+    background-color: #e5edff;
+    border-radius: 20px;
+    height: 50%;
+}
+
+.weather-title,
+.weather-site {
+    color: #001c39;
+}
+</style>
