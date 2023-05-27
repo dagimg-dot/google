6dc26a4d85306cd9435459a15a3d66de8ca15904
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-27 08:26:01 +0300

    create weather data component for weather card

---
+++ src/components/WeatherData.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <div class="day">Sat</div>
+        <div class="flex mt-1">
+            <div class="flex">
+                <svg focusable="false" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24"
+                    xml:space="preserve">
+                    <g id="Hs8Med">
+                        <rect y="0" fill="none" width="24" height="24"></rect>
+                    </g>
+                    <g id="PNIarc">
+                        <g>
+                            <path
+                                d="M17.92,7.01C17.45,4.17,14.97,1.99,12,1.99c-2.18,0-4.17,1.18-5.22,3.06C4.09,5.41,2,7.73,2,10.49 c0,3.03,2.47,5.5,5.5,5.5h10c2.48,0,4.5-2.02,4.5-4.5C22,9.15,20.21,7.23,17.92,7.01z M17.5,13.99h-10c-1.93,0-3.5-1.57-3.5-3.5 C4,8.6,5.54,7.04,7.44,7l0.64-0.01L8.33,6.4C8.97,4.94,10.41,3.99,12,3.99c2.21,0,4,1.79,4,4v1h1.5c1.38,0,2.5,1.12,2.5,2.5 S18.88,13.99,17.5,13.99z">
+                            </path>
+                            <path
+                                d="M12.89,17.56c-0.25-0.49-0.85-0.69-1.34-0.45c-0.49,0.25-0.69,0.85-0.45,1.34l1.5,3c0.19,0.39,0.75,0.74,1.34,0.45 c0.49-0.25,0.69-0.85,0.45-1.34L12.89,17.56z">
+                            </path>
+                            <path
+                                d="M18.89,17.56c-0.25-0.49-0.85-0.69-1.34-0.45c-0.49,0.25-0.69,0.85-0.45,1.34l1.5,3c0.19,0.39,0.75,0.74,1.34,0.45 c0.49-0.25,0.69-0.85,0.45-1.34L18.89,17.56z">
+                            </path>
+                            <path
+                                d="M6.89,17.56c-0.25-0.49-0.85-0.69-1.34-0.45c-0.49,0.25-0.69,0.85-0.45,1.34l1.5,3c0.19,0.39,0.75,0.74,1.34,0.45 c0.49-0.25,0.69-0.85,0.45-1.34L6.89,17.56z">
+                            </path>
+                        </g>
+                    </g>
+                </svg>
+            </div>
+            <div class="degree flex items-end pl-1">
+                30°
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    svg {
+        fill: #001c39;
+        width: 30px;
+        height: 30px;
+    }
+
+    .day, .degree {
+        color: #001c39;
+    }
+
+    div {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
