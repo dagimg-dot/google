87d2456a2812f4f386a25f9b505d53a54c6fac76
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-27 08:24:25 +0300

    create gridbutton component

--- src/components/GridButton.vue
+++ src/components/GridButton.vue
@@ -1,7 +1,65 @@
 <template>
-    <a href="#">
-        <div class="w-10 h-10 flex items-center justify-center">
-            <img src="../assets/9dots.svg" class="w-5 h-4">
-        </div>
-    </a>
+    <div class="w-11 h-11 flex items-center justify-center">
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+            y="0px" width="612px" height="612px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;"
+            xml:space="preserve">
+            <g>
+                <g>
+                    <circle cx="69.545" cy="528.545" r="69.545" />
+                    <circle cx="306" cy="306" r="69.545" />
+                    <circle cx="306" cy="83.455" r="69.545" />
+                    <circle cx="69.545" cy="83.455" r="69.545" />
+                    <circle cx="69.545" cy="306" r="69.545" />
+                    <circle cx="306" cy="528.545" r="69.545" />
+                    <circle cx="542.454" cy="528.545" r="69.545" />
+                    <circle cx="542.454" cy="83.455" r="69.545" />
+                    <circle cx="542.454" cy="306" r="69.545" />
+                </g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+        </svg>
+    </div>
 </template>
+
+<style scoped>
+svg {
+    height: 19px;
+    width: 19px;
+    fill: #324043;
+    opacity: 0.6;
+}
+
+div:hover {
+    background-color: #f1f3f4;
+    border-radius: 50%;
+}
+</style>
